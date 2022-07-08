c532784116fa6815ceeb154a3ec0ed68949fa828
Naru-hub <1208ny0324@gmail.com>
2022-07-08 21:36:57 +0900

    ルーティング設定でエンドポイントを変更

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
+const userRoute = require("./routes/users");
 const PORT = 3000;
+
+// ミドルウェア
+app.use("/api/users", userRoute);
 
 app.get("/", (req, res) => {
   res.send("hello express");
