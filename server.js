d661ae6a6017edae1e65ab2654e926d089b0c49b
Naru-hub <1208ny0324@gmail.com>
2022-07-31 16:17:52 +0900

    静的アセットをバックエンド側に移行

--- server.js
+++ server.js
@@ -4,8 +4,9 @@ const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
 const postRoute = require("./routes/posts");
 const uploadRoute = require("./routes/upload");
-const PORT = 6000;
+const PORT = 4000;
 const mongoose = require("mongoose");
+const path = require("path");
 require("dotenv").config();
 
 // データベース接続
@@ -19,6 +20,7 @@ mongoose
   });
 
 // ミドルウェア
+app.use("/images", express.static(path.join(__dirname, "public/images")));
 app.use(express.json());
 app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
